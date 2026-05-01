8c09345978af5d9a708d9bc55e038f167e9fcb8f
mer-mango <meredith@empowerhealthstrategies.com>
2026-05-01 17:22:28 -0400

    Update ContactProfileModal.tsx

---
+++ ContactProfileModal.tsx
@@ -0,0 +1,8 @@
+// Updated content of ContactProfileModal.tsx
+import React from 'react';
+
+const ContactProfileModal = () => {
+    // Modal implementation
+};
+
+export default ContactProfileModal;
